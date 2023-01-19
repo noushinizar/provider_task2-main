216e08844993725890cb9238cc65d46ff9524182
noushinizar <100755008+noushinizar@users.noreply.github.com>
2023-01-19 15:31:00 +0530

    Initial commit

---
+++ provider_task2-main/android/app/src/main/java/com/example/groceries_favorites_provider/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.groceries_favorites_provider;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ provider_task2-main/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'design/home_page.dart';
+import 'provider/provider_class.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ProviderPage(),
+      child: MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.brown),
+        debugShowCheckedModeBanner: false,
+        home: GroceriesList(),
+      ),
+    ),
+  );
+}
